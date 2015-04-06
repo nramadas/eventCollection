8f9f8608460cea2ff15180759ebedb4cf5fbdc27
Niranjan Ramadas <niranjan@Niranjans-MacBook-Air.local>
2015-04-05 20:14:35 -0700

    Finish out basic functionality

--- eventCollection.js
+++ eventCollection.js
@@ -1,24 +1,36 @@
 window.EventCollection = (function() {
-    function Collection(type) {
-        /* private */
+    /*
+    * Collection represents an event stream
+    * @class
+    */
+    var Collection = function() {
         var successMethods = [],
             errorMethods = [],
+            completeMethods = [],
             disabled = false,
             throttleInterval = null,
             debounceInterval = null,
             debounceTimer = null,
-            eventQueue = [];
+            eventQueue = [],
+            deconstructor = null;
 
-        if (!type) {
-            type = "default";
-        }
-
+        /**
+        * Given an array of functions, send an event to each function
+        * @private
+        * @param {Array} fnArray
+        * @param {Event} event
+        **/
         var sendEvent = function(fnArray, event) {
             for(var i = 0; i < fnArray.length; i++) {
                 fnArray[i].call(null, event);
             }
         }
 
+        /**
+        * Use a collection to process all the queued events
+        * @private
+        * @param {Collection} newCollection
+        **/
         var processEventQueue = function(newCollection) {
             if (!eventQueue.length) {
                 return
@@ -26,29 +38,43 @@ window.EventCollection = (function() {
 
             var handleNextEvent = function() {
                 eventQueue.shift();
-                processEventQueue();
+                processEventQueue(newCollection);
             }
 
-            var processSuccess = function(event) {
+            var processQueuedEventsSuccess = function(event) {
                 newCollection.acceptEventSucess(event);
-                handleNextEvent();
+                /* process success does not move on to the next QueuedEvent
+                   event until the QueuedEvent completes. this allows for
+                   flattening multiple Collection streams */
             }
 
-            var processError = function(event) {
+            var processQueuedEventsError = function(event) {
                 newCollection.acceptEventError(event);
                 handleNextEvent();
             }
 
+            var processQueuedEventsComplete = function(event) {
+                newCollection.acceptEventComplete(event);
+                handleNextEvent();
+            }
+
             var event = eventQueue[0];
-            if (event.type === "promise") {
-                event.event.done(processSuccess);
-                event.event.fail(processError);
+            if (event.type === "queue") {
+                event.event.progress(processQueuedEventsSuccess);
+                event.event.fail(processQueuedEventsError);
+                event.event.done(processQueuedEventsComplete);
             } else {
-                processSuccess(event.event);
+                newCollection.acceptEventSucess(event.event);
+                handleNextEvent();
             }
         }
 
-        /* priveledged */
+        /**
+        * Pass along a successful event. Throttle or debounce the sending of
+        * the event if necessary.
+        * @privileged
+        * @param {Event} event
+        **/
         this.acceptEventSucess = function(event) {
             runEvent = function() {
                 if (!disabled) {
@@ -71,38 +97,77 @@ window.EventCollection = (function() {
             }
         }
 
+        /**
+        * Pass along an error event. There is no throttling or debouncing.
+        * @privileged
+        * @param {Event} event
+        **/
         this.acceptEventError = function(event) {
             sendEvent(errorMethods, event);
         }
 
-        this.forEach = function(successFn, errorFn) {
+        /**
+        * Pass along an completed event. There is no throttling or debouncing.
+        * Dissolve the collection as well.
+        * @privileged
+        * @param {Event} event
+        **/
+        this.acceptEventComplete = function(event) {
+            sendEvent(completeMethods, event);
+            this.dissolve();
+        }
+
+        /**
+        * Iterate over each event in the collection as they arrive, and apply
+        * the functions provided.
+        * @privileged
+        * @param {function} successFn
+        * @param {function=} errorFn
+        * @param {function=} completeFn
+        **/
+        this.forEach = function(successFn, errorFn, completeFn) {
             successMethods.push(successFn);
             if (errorFn) {
                 errorMethods.push(errorFn);
             }
-        };
-
-        this.inspect = function() {
-            return {
-                successMethods: successMethods,
-                errorMethods: errorMethods,
-                disabled: disabled,
-                throttleInterval: throttleInterval,
-                debounceInterval: debounceInterval,
-                debounceTimer: debounceTimer
+            if (completeFn) {
+                completeMethods.push(completeFn);
             }
         };
 
+        /**
+        * Given a throttle interval, fire successful event *only* at the
+        * throttle rate.
+        * @privileged
+        * @param {number} newThrottleInterval
+        * @returns {Collection} this
+        **/
         this.throttle = function(newThrottleInterval) {
             throttleInterval = newThrottleInterval;
             return this;
         };
 
+        /**
+        * Given a debounce interval, fire the last event to occur after no new
+        * events occure during the debounce interval. Everything a new event is
+        * fired, the debounce interval is run again.
+        * @privileged
+        * @param {number} newDebounceInterval
+        * @returns {Collection} this
+        **/
         this.debounce = function(newDebounceInterval) {
             debounceInterval = newDebounceInterval;
             return this;
         };
 
+        /**
+        * For each event, flatten the event if its an array or another
+        * collection. In the case of an array, just fire each event in the
+        * array. If it's a collection, we want to wait for the collection to
+        * complete before we fire the next event to be flattened.
+        * @privileged
+        * @returns {Collection} newCollection;
+        **/
         this.flatten = function() {
             var newCollection = new Collection();
             this.forEach(function(event) {
@@ -112,8 +177,8 @@ window.EventCollection = (function() {
                     }
                 } else if (event instanceof Collection) {
                     var d = $.Deferred();
-                    event.forEach(d.resolve, d.reject);
-                    eventQueue.push({type: "promise", event: d.promise()});
+                    event.forEach(d.notify, d.reject, d.resolve);
+                    eventQueue.push({type: "queue", event: new QueuedEvents(d)});
                 } else {
                     eventQueue.push({type: "default", event: event});
                 }
@@ -121,10 +186,37 @@ window.EventCollection = (function() {
             });
             return newCollection;
         };
+
+        /**
+        * Set a function to be called when the Collection is dissolved
+        * @privileged
+        * @params {function} fn
+        **/
+        this.setDeconstructor = function(fn) {
+            deconstructor = fn;
+        };
+
+        /**
+        * If a deconstructor function exists, run it.
+        * @privileged
+        **/
+        this.dissolve = function() {
+            if (deconstructor) {
+                deconstructor();
+            }
+        }
     };
 
-    /* public */
-    Collection.prototype.map = function(successFn, errorFn) {
+    /**
+    * Return a new collection where the events are modified based on the input
+    * map functions
+    * @public
+    * @param {function} successFn
+    * @param {function=} errorFn
+    * @param {function=} completeFn
+    * @returns {Collection} newCollection
+    **/
+    Collection.prototype.map = function(successFn, errorFn, completeFn) {
         var newCollection = new Collection();
         this.forEach(function(event) {
             /* handle a successful event */
@@ -134,11 +226,26 @@ window.EventCollection = (function() {
             if(errorFn) {
                 newCollection.acceptEventError(errorFn(event));
             }
+        }, function(event) {
+            /* handle complete event */
+            if (completeFn) {
+                newCollection.acceptEventComplete(completeFn(event));
+            }
         });
         return newCollection;
     };
 
-    Collection.prototype.filter = function(successFn, errorFn) {
+    /**
+    * Return a new collection where the events in the new collection are those
+    * that pass the provided filter functions. Filters only apply to successful
+    * events.
+    * @public
+    * @param {function} successFn
+    * @param {function=} errorFn
+    * @param {function=} completeFn
+    * @returns {Collection} newCollection
+    **/
+    Collection.prototype.filter = function(successFn, errorFn, completeFn) {
         var newCollection = new Collection();
         this.forEach(function(event) {
             /* handle a successful event */
@@ -150,11 +257,112 @@ window.EventCollection = (function() {
             if(errorFn) {
                 newCollection.acceptEventError(event);
             }
+        }, function(event) {
+            /* handle complete event */
+            if (completeFn) {
+                newCollection.acceptEventComplete(event);
+            }
         });
         return newCollection;
     };
 
+    /**
+    * Send an "event complete" even when another collection fires a success
+    * event
+    * @public
+    * @param {Collection} stopCollection
+    * @returns {Collection} this
+    **/
+    Collection.prototype.stopWhen = function(stopCollection) {
+        var that = this;
+        stopCollection.forEach(function(event) {
+            that.acceptEventComplete([]);
+        });
+        return this;
+    };
+
+    /**
+    * @class
+    **/
+    var QueuedEvents = function(promise) {
+        var queue = [],
+            debuffered = false,
+            progressCb = null;
+
+        promise.progress(function(event) {
+            if (!debuffered) {
+                queue.push(event);
+            } else if (progressCb) {
+                progressCb(event);
+            }
+        });
+
+        /**
+        * For each event in the queue, call the provided progressCb
+        * @private
+        **/
+        var processEventQueue = function() {
+            var event = queue.shift();
+            progressCb(event);
+            if (queue.lenght) {
+                processEventQueue();
+            }
+        };
+
+        /*
+        * Set the callback to be called when a success event occurs. If there is
+        * a queue already, start processing it. Otherwise, start listening for
+        * new events to send immediately.
+        * @privileged
+        * @param {function} newProgressCb
+        */
+        this.progress = function(newProgressCb) {
+            progressCb = newProgressCb;
+            if (queue.length) {
+                processEventQueue();
+            } else {
+                debuffered = true;
+            }
+        };
+
+        /*
+        * Set the callback to be called when the entire queue is processed.
+        * @privileged
+        * @param {function} resolveCb
+        */
+        this.done = function(resolveCb) {
+            promise.done(function(event) {
+                if (queue.length) {
+                    processEventQueue();
+                }
+                resolveCb(event);
+            });
+        };
+
+        /*
+        * Set the callback to be called when there is failure in the queue.
+        * @privileged
+        * @param {function} failCb
+        */
+        this.fail = function(failCb) {
+            promise.fail(function(event) {
+                if (queue.length) {
+                    processEventQueue();
+                }
+                failCb(event);
+            });
+        };
+    };
+
     return {
+        /**
+        * Creates an event collection based on a jQuery event
+        * @public
+        * @param {jQuery object} $domElement
+        * @param {String} action
+        * @param {String} selector
+        * @returns {Collection} collection
+        **/
         createFromDomEvent: function($domElement, action, selector) {
             var collection = new Collection();
             var handlerArgs = [];
@@ -166,9 +374,18 @@ window.EventCollection = (function() {
                 collection.acceptEventSucess(event);
             });
             $domElement.on.apply($domElement, handlerArgs);
+            collection.setDeconstructor(function() {
+                $domElement.off.apply($domElement, handlerArgs);
+            });
             return collection;
         },
 
+        /**
+        * Creates an event collection using a jQuery ajax call
+        * @public
+        * @param {jquery Ajax} ajaxCall
+        * @returns {Collection} collection
+        **/
         createFromAjax: function(ajaxCall) {
             var collection = new Collection()
             ajaxCall.done(function(data) {
@@ -176,6 +393,9 @@ window.EventCollection = (function() {
             });
             ajaxCall.fail(function(error) {
                 collection.acceptEventError(error);
+            });
+            ajaxCall.always(function(event) {
+                collection.acceptEventComplete(event);
             });
             return collection;
         }
